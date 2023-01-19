72e70300ab4874d594547529db783635e424fb43
sbk930123 <sbk930123@naver.com>
2023-01-19 17:26:40 +0900

    ✅

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
-$('.gnb-item').on('click', function() {
-  $(this).children('section').addClass('on');
-  $('.overlay').addClass('on');
-});
+function setMenu(value) {
+  if (value == 'tab1') {
+    document.getElementById('menu1').style.display = 'flex';
+    document.getElementById('overlaybox').style.display = 'block';
+    document.getElementById('menu2').style.display = 'none';
+  };
 
-$('.overlay').on('click', function() {
-  $('.gnb-item-menu').removeClass('on');
-  $('.overlay').removeClass('on');
-})
+  if (value == 'tab2') {
+    document.getElementById('menu1').style.display = 'none';
+    document.getElementById('menu2').style.display = 'flex';
+    document.getElementById('overlaybox').style.display = 'block';
+  };
+
+  if (value == 'overlayon')
+  document.getElementById('menu1').style.display = 'none';
+  document.getElementById('menu2').style.display = 'none';
+  document.getElementById('overlaybox').style.display = 'none';
+};
+
+
+
+var current = 0;
+var count = $(".slide").length;
+
+function nav(dir){
+  current = current + dir;
+  if(current >= count){
+    current = 0;
+  }else if(current < 0){
+    current = count-1;
+  }
+
+  $(".slide").fadeOut(700);
+  $(".slide").eq(current).fadeIn(700);
+};
+
+setInterval(function(){
+  nav(1);
+}, 1500);
